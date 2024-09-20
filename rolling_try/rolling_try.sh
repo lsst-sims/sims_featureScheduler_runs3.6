39e0d96c9963bf308c3b6ddd6cc23d895af21214
Peter Yoachim <yoachim@uw.edu>
2024-09-20 14:59:52 -0700

    rolling

---
+++ rolling_try/rolling_try.sh
@@ -0,0 +1,8 @@
+python rolling_try.py --n_cycles 3 --n_start 2 --temp_uniform 0
+python rolling_try.py --n_cycles 3 --n_start 3 --temp_uniform 0
+python rolling_try.py --n_cycles 4 --n_start 2 --temp_uniform 0
+python rolling_try.py --n_cycles 4 --n_start 3 --temp_uniform 0
+python rolling_try.py --n_cycles 3 --n_start 2 --temp_uniform 1
+python rolling_try.py --n_cycles 3 --n_start 3 --temp_uniform 1
+python rolling_try.py --n_cycles 4 --n_start 2 --temp_uniform 1
+python rolling_try.py --n_cycles 4 --n_start 3 --temp_uniform 1
